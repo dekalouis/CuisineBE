ec382bd726ac35eb6720c2f10f04db50db21f864
dekalouis <dekarebaudo@gmail.com>
2025-02-10 17:56:37 +0700

    feat: completed CRUD functionality for main entity table (cuisines)

--- controllers/cuisineController.js
+++ controllers/cuisineController.js
@@ -34,6 +34,14 @@ class CuisineController {
 
   static async getCuisineById(req, res) {
     try {
+      const { id } = req.params;
+      const cuisineById = await Cuisine.findByPk(id);
+      if (!cuisineById) {
+        res.status(404).json({ message: `Cuisine id:${id} not found.` });
+        return;
+      }
+
+      res.status(200).json(cuisineById);
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: `Internal Server Error.` });
@@ -42,14 +50,40 @@ class CuisineController {
 
   static async updateCuisineById(req, res) {
     try {
+      const { id } = req.params;
+      const cuisineById = await Cuisine.findByPk(id);
+      if (!cuisineById) {
+        res.status(404).json({ message: `Cuisine id:${id} not found.` });
+        return;
+      }
+      await cuisineById.update(req.body);
+      //   console.log(cuisineById);
+      //   res.status(200).json(cuisineById);
+      res.json({ message: `Cuisine ${cuisineById.name} updated.` });
     } catch (err) {
       console.log(err);
-      res.status(500).json({ message: `Internal Server Error.` });
+      //dikasih validasi lagi in case user iseng ngosongin wkwkwk
+      if (err.name === "SequelizeValidationError") {
+        res.status(400).json({ message: err.errors[0].message });
+      } else {
+        res.status(500).json({ message: `Internal Server Error.` });
+      }
     }
   }
 
   static async deleteCuisineById(req, res) {
     try {
+      const { id } = req.params;
+      const cuisineById = await Cuisine.findByPk(id);
+      if (!cuisineById) {
+        res.status(404).json({ message: `Cuisine id:${id} not found.` });
+        return;
+      }
+
+      await cuisineById.destroy();
+      res
+        .status(200)
+        .json({ message: `Cuisine ${cuisineById.name} successfully deleted` });
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: `Internal Server Error.` });
