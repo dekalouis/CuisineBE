f685cf300457c121f4ec15d30211d3f6c469971a
dekalouis <dekarebaudo@gmail.com>
2025-02-19 11:48:54 +0700

    fix: adding cors to allow frontend retrieval

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const app = express();
 const routes = require("./routers");
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
