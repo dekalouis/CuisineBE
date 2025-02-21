e931c0bbef5e6641050b387a32ed906175227900
dekalouis <dekarebaudo@gmail.com>
2025-02-21 12:43:22 +0700

    feat: add public category route

--- controllers/cuisineController.js
+++ controllers/cuisineController.js
@@ -189,6 +189,15 @@ class CuisineController {
     }
   }
 
+  static async getPublicCategories(req, res, next) {
+    try {
+      const categories = await Category.findAll();
+      res.status(200).json(categories);
+    } catch (err) {
+      next(err);
+    }
+  }
+
   static async getPublicCuisine(req, res, next) {
     try {
       console.log(req.query);
--- routers/pubRouter.js
+++ routers/pubRouter.js
@@ -4,5 +4,6 @@ const CuisineController = require("../controllers/cuisineController");
 
 router.get("/cuisines", CuisineController.getPublicCuisine);
 router.get("/cuisines/:id", CuisineController.getPublicCuisineById);
+router.get("/categories", CuisineController.getPublicCategories);
 
 module.exports = router;
