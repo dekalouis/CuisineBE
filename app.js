b6934a5fda05cd4efa4d1209a3e321e323607e54
dekalouis <dekarebaudo@gmail.com>
2025-02-21 17:07:14 +0700

    fix: added cors for all

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 3000;
 
 const cors = require("cors");
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE,PATCH",
-    allowedHeaders: "Content-Type,Authorization",
-  })
+  cors()
+  // cors({
+  //   origin: "http://localhost:5173",
+  //   methods: "GET,POST,PUT,DELETE,PATCH",
+  //   allowedHeaders: "Content-Type,Authorization",
+  // })
 );
 
 app.use(express.urlencoded({ extended: false }));
